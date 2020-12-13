e14cd938a0ee922744b2cdb841cf4699719a515e
Arish Shah <arish0246@gmail.com>
2020-12-13 17:49:41 +0530

    change output directory name

--- static-site-generator/src/write.js
+++ static-site-generator/src/write.js
@@ -5,7 +5,7 @@ function write(pages) {
   console.info("Writing Pages...");
   for (let [name, content] of Object.entries(pages)) {
     fs.writeFileSync(
-      path.join(__dirname, "..", "dist", name + ".html"),
+      path.join(__dirname, "..", "build", name + ".html"),
       content
     );
   }
